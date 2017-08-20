002f1180bda6c48f0c0c34df6cdbd9bf92a544f2
Ignas <ignasmaslinskas@gmail.com>
2017-08-20 18:48:18 +0300

    Initial commit real

---
+++ ChatServer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using CoreServer;
+
+
+namespace ChatServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HServer server = new HServer(4000);
+            server.Run();
+        }
+    }
+}
